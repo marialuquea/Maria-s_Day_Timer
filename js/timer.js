da0554f95a275b61dd3c637deb93e03d8c3d344f
Maria Luque Anguita <mluqueanguita@gmail.com>
2020-04-10 22:08:07 +0200

    finished to run 30 sec, 10 sec, 6 times

--- js/timer.js
+++ js/timer.js
@@ -1,42 +1,178 @@
-
 var countdown;
+var countdown2;
 var countdown_exercise;
-var countdown_descanso;
-
-function alarm() {
-  //var audio = new Audio();
-  //audio.src = "sounds/alarm.mp3";
-  //audio.play()
-
-  var myAudio = new Audio('sounds/alarm.mp3');
-  myAudio.play();
-}
+var countdown_rest;
+var audio = new Audio("sounds/alarm.mp3");
+var done = false;
+var count = 0
 
 function countdown_trigger() {
-    var done = false;
-    if (countdown_exercise > 0) {
+    if (countdown_exercise > 0 && count === 0) {
         countdown_exercise--;
         document.getElementById('countdown_text').innerHTML = countdown_exercise;
         if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
         if (countdown_exercise === 0) {
-          audio.play()
+          audio.play();
+          console.log("termina ejercicio");
+          document.getElementById('countdown_text').innerHTML = 'Ejercicio';
           done = true;
         }
       }
-
-      if (countdown_descanso > 0) && (done == true) {
-        countdown_descanso--;
-        document.getElementById('countdown_text').innerHTML = countdown_descanso;
-        if (countdown_descanso > 0) { countdown = setTimeout(countdown_trigger, 1000); }
-        if (countdown_descanso === 0) { audio.play() }
+    if (countdown_rest > 0 && done === true && count === 0) {
+        countdown_rest--;
+        document.getElementById('descanso').innerHTML = countdown_rest;
+        if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+        if (countdown_rest === 0) {
+          audio.play();
+          console.log("termina descanso");
+          document.getElementById('descanso').innerHTML = 'Descanso';
+          done = false;
+          count++;
+          countdown_exercise = 31;
+          countdown_rest = 11;
+        }
       }
 
+
+      if (countdown_exercise > 0 && count === 1) {
+          countdown_exercise--;
+          document.getElementById('countdown_text').innerHTML = countdown_exercise;
+          if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
+          if (countdown_exercise === 0) {
+            audio.play();
+            console.log("termina ejercicio");
+            document.getElementById('countdown_text').innerHTML = 'Ejercicio';
+            done = true;
+          }
+        }
+      if (countdown_rest > 0 && done === true && count === 1) {
+          countdown_rest--;
+          document.getElementById('descanso').innerHTML = countdown_rest;
+          if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+          if (countdown_rest === 0) {
+            audio.play();
+            console.log("termina descanso");
+            document.getElementById('descanso').innerHTML = 'Descanso';
+            done = false;
+            count++;
+            countdown_exercise = 31;
+            countdown_rest = 11;
+          }
+        }
+
+
+      if (countdown_exercise > 0 && count === 2) {
+          countdown_exercise--;
+          document.getElementById('countdown_text').innerHTML = countdown_exercise;
+          if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
+          if (countdown_exercise === 0) {
+            audio.play();
+            console.log("termina ejercicio");
+            document.getElementById('countdown_text').innerHTML = 'Ejercicio';
+            done = true;
+          }
+        }
+      if (countdown_rest > 0 && done === true && count === 2) {
+          countdown_rest--;
+          document.getElementById('descanso').innerHTML = countdown_rest;
+          if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+          if (countdown_rest === 0) {
+            audio.play();
+            console.log("termina descanso");
+            document.getElementById('descanso').innerHTML = 'Descanso';
+            done = false;
+            count++;
+            countdown_exercise = 31;
+            countdown_rest = 11;
+          }
+        }
+
+
+      if (countdown_exercise > 0 && count === 3) {
+          countdown_exercise--;
+          document.getElementById('countdown_text').innerHTML = countdown_exercise;
+          if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
+          if (countdown_exercise === 0) {
+            audio.play();
+            console.log("termina ejercicio");
+            document.getElementById('countdown_text').innerHTML = 'Ejercicio';
+            done = true;
+          }
+        }
+      if (countdown_rest > 0 && done === true && count === 3) {
+          countdown_rest--;
+          document.getElementById('descanso').innerHTML = countdown_rest;
+          if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+          if (countdown_rest === 0) {
+            audio.play();
+            console.log("termina descanso");
+            document.getElementById('descanso').innerHTML = 'Descanso';
+            done = false;
+            count++;
+            countdown_exercise = 31;
+            countdown_rest = 11;
+          }
+        }
+
+      if (countdown_exercise > 0 && count === 4) {
+          countdown_exercise--;
+          document.getElementById('countdown_text').innerHTML = countdown_exercise;
+          if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
+          if (countdown_exercise === 0) {
+            audio.play();
+            console.log("termina ejercicio");
+            document.getElementById('countdown_text').innerHTML = 'Ejercicio';
+            done = true;
+          }
+        }
+      if (countdown_rest > 0 && done === true && count === 4) {
+          countdown_rest--;
+          document.getElementById('descanso').innerHTML = countdown_rest;
+          if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+          if (countdown_rest === 0) {
+            audio.play();
+            console.log("termina descanso");
+            document.getElementById('descanso').innerHTML = 'Descanso';
+            done = false;
+            count++;
+            countdown_exercise = 31;
+            countdown_rest = 11;
+          }
+        }
+      if (countdown_exercise > 0 && count === 5) {
+          countdown_exercise--;
+          document.getElementById('countdown_text').innerHTML = countdown_exercise;
+          if (countdown_exercise > 0) { countdown = setTimeout(countdown_trigger, 1000); }
+          if (countdown_exercise === 0) {
+            audio.play();
+            console.log("termina ejercicio");
+            document.getElementById('countdown_text').innerHTML = 'Ejercicio';
+            done = true;
+          }
+        }
+      if (countdown_rest > 0 && done === true && count === 5) {
+          countdown_rest--;
+          document.getElementById('descanso').innerHTML = countdown_rest;
+          if (countdown_rest > 0) { countdown2 = setTimeout(countdown_trigger, 1000); }
+          if (countdown_rest === 0) {
+            audio.play();
+            console.log("termina descanso");
+            document.getElementById('descanso').innerHTML = 'Descanso';
+            done = false;
+            count++;
+            countdown_exercise = 31;
+            countdown_rest = 11;
+          }
+        }
 }
 
-function countdown_clear() { clearTimeout(countdown); }
+function countdown_clear() {
+  clearTimeout(countdown);
+  clearTimeout(countdown2);
+}
 
 function countdown_init() {
-    countdown_exercise = 5;
-    countdown_descanso = 3;
+    countdown_exercise = 31;
+    countdown_rest = 11;
     countdown_trigger();
-  }
+}
